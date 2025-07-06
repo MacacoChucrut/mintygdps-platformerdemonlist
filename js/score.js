c6a26a756840886176be5c2403a9571aaaeef244
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2025-07-06 12:12:47 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -15,7 +15,7 @@ export function score(rank, percent, minPercent) {
         return 0;
     }
 
-let score = (-50*Math.pow(rank-1, 0.5) + 300) *
+let score = (-50*Math.pow(rank-1, 0.9) + 300) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
